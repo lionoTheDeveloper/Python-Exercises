038044d069b89fe2cd858446f0074f05836d4ff6
lionoTheDeveloper <lionothedeveloper@gmail.com>
2023-06-02 16:29:55 +0300

    https://www.opera.com/client/upgraded?

--- exercises.py
+++ exercises.py
@@ -1,7 +1,34 @@
-print('hello')
-txt = 'my name is ståle'
-print(txt.encode(encoding='ascii',errors='backslashreplace'))
-print(txt.encode(encoding='ascii',errors='ignore'))
-print(txt.encode(encoding='ascii',errors='namereplace'))
-print(txt.encode(encoding='ascii',errors='replace'))
-print(txt.encode(encoding='ascii',errors='xmlcharrefreplace'))
+#Python String encode() Method
+# txt = 'my name is ståle'
+# print(txt.encode(encoding='ascii',errors='backslashreplace'))
+# print(txt.encode(encoding='ascii',errors='ignore'))
+# print(txt.encode(encoding='ascii',errors='namereplace'))
+# print(txt.encode(encoding='ascii',errors='replace'))
+# print(txt.encode(encoding='ascii',errors='xmlcharrefreplace'))
+
+# Python String count() Method
+# txt = 'i love apples, apple are my favorite fruit'
+# x = txt.count('apple')
+# print(x)
+# x = txt.count('apple',10,24)
+# print(x)
+
+# Python String capitalize() Method
+# txt = 'hello, and welcome to my world'
+# x = txt.capitalize()
+# print(x)
+# txt = 'python is FUN!'
+# x = txt.capitalize()
+# print(x)
+
+# Python String casefold() Method
+# txt = 'hello, and welcome to my world'
+# x = txt.casefold()
+# print(x)
+
+# Python String center() Method
+# txt = 'banana'
+# x = txt.center(20)
+# print(x)
+# x = txt.center(20,'O')
+# print(x)
